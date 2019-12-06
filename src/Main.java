99bfd1e1ee15b62c9159581276e7f39abcd67570
dmshch <freebsdforever@gmail.com>
2019-12-06 16:04:45 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,22 @@
+import java.util.Random;
+
 public class Main {
-    
+
+    static final Integer m[][] = new Integer[9][9];
+
+    public static void main(String[] args){
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                m[i][j] = new Random().nextInt(10);
+            }
+        }
+
+        for (int i = 0; i < 9; i++){
+            System.out.println(" ");
+            for (int j =0; j<9; j++){
+                System.out.print(m[i][j] + " ");
+            }
+        }
+
+    }
 }
